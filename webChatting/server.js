198a692f252036d177d0618283026faf19e62409
nap2bi <npa2bi@gmail.com>
2017-11-06 19:15:46 +0900

    앞에 채팅방 리스트 개발중
    앞단 채팅방 리스트 개발중

--- webChatting/server.js
+++ webChatting/server.js
@@ -3,32 +3,48 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get("/",function(req, res){
-  res.sendfile("client.html");
-});
-
-app.get("/sound/",function(req, res){
-  res.sendfile("Track_68.mp3");
-});
-
 var count=1;
+var userlist = new Array();
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get("/",function(req, res){
+    res.sendfile("chatRoomList.html");
+});
+
+app.get("/chattingRoom/",function(req, res){
+    var roomnum = decodeURIComponent(req.query.roomnum);
+    var roompw = decodeURIComponent(req.query.roompw);
+    var nicknm = decodeURIComponent(req.query.nicknm);
+    console.log('[ '+ nicknm + ' ] 님이 --' + roomnum +'-- 방을 선택하였습니다.');
+    res.sendfile("client.html");
+});
+
 io.on('connection', function(socket){
-  console.log('user connected: ', socket.id);
-  var name = "user" + count++;
-  io.to(socket.id).emit('change name',name);
-  //io.to(socket.id).emit('comein name', '누군가 입장하였습니다.');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected: ', socket.id);
-    io.to(socket.id).emit('disconnect chattingRoom','누군가 퇴장하였습니다.');
-  });
+    console.log('user connected: ', socket.id);
+    var name = "user" + count++;
+    userlist.push(socket.id);
+    io.to(socket.id).emit('change name',name);
+    console.log('user list: ', userlist);
 
-  socket.on('send message', function(name,text){
-    //var msg = name + ' : ' + text;
-    var msg = text;
-    console.log(msg);
-    io.emit('receive message', msg);
-  });
+    socket.on('disconnect', function(){
+        console.log('user disconnected: ', socket.id);
+        io.emit().emit('receive message','누군가 퇴장하였습니다.');
+        var idx = userlist.indexOf(socket.id);
+        if (idx > -1){
+            userlist.splice(idx, 1);
+        }
+        console.log('user list: ', userlist);
+
+    });
+
+    socket.on('send message', function(name,text){
+        //var msg = name + ' : ' + text;
+        var msg = text;
+        console.log(msg);
+        io.emit('receive message', msg);
+    });
 });
 
 http.listen('3100', function(){
